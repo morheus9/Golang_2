4a378987c3f9c9c47d04dd16a953232916fccabf
Vyacheslav <nodegopher@gmail.com>
2022-04-01 18:03:30 +0300

    added postgres

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {}
 //Server starts server on 127.0.0.1:8080/
 func Server() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "Start Page") })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "44e") })
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "About Page") })
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
