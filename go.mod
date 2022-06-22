491cf677cf71ac7eaf8bfe86beac9faa194f7286
Vyacheslav <nodegopher@gmail.com>
2022-06-22 20:16:23 +0300

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/morheus9/Golang_2
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,18 +2,25 @@ package server
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {}
-
-//Server starts server on 127.0.0.1:8080/
 func Server() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "44e") })
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "About Page") })
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/8host\n</h1>")
+	})
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
+	})
+	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["name"]
+		fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
+	})
+	http.ListenAndServe(":80", r)
 }
+
+//Server starts server on 127.0.0.1:80/
