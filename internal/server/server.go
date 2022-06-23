e101a101ebf970cb7c8866c6800c5b65b3780b15
Vyacheslav <nodegopher@gmail.com>
2022-06-23 20:43:31 +0300

    fix

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,26 +1,24 @@
 package server
 
 import (
-	"html/template"
-	"log"
 	"net/http"
+	"text/template"
 )
 
-func static(w http.ResponseWriter, _ *http.Request) {
+//Server starts server on 127.0.0.1:8081/
+func Server() {
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.ListenAndServe(":8081", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
 	var tmplt = template.Must(template.ParseFiles("./static/index.html"))
 	tmplt.Execute(w, nil)
 }
 
-func about(w http.ResponseWriter, _ *http.Request) {
+func about(w http.ResponseWriter, r *http.Request) {
 	var tmplt = template.Must(template.ParseFiles("./static/about.html"))
 	tmplt.Execute(w, nil)
-}
-
-//Server starts server on 127.0.0.1:8080/
-func Server() {
-
-	http.HandleFunc("/", static)
-	http.HandleFunc("/about", about)
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
 }
